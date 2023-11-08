217be438b7724abd6698e5e7748cdb1090da1027
Monukumarsharma <144675020+Monukumarsharma@users.noreply.github.com>
2023-11-08 16:58:24 +0530

    added

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
-let likeCount = 0;
-let dislikeCount = 0;
+class Post:
+    def __init__(self, content):
+        self.content = content
+        self.likes = 0
+        self.dislikes = 0
+        self.comments = []
 
-function like() {
-    likeCount++;
-    document.getElementById("like-count").innerText = likeCount;
-}
+    def like(self):
+        self.likes += 1
 
-function dislike() {
-    dislikeCount++;
-    document.getElementById("dislike-count").innerText = dislikeCount;
-}
+    def dislike(self):
+        self.dislikes += 1
 
-function addComment() {
-    const commentInput = document.getElementById("comment-input");
-    const commentText = commentInput.value.trim();
+    def add_comment(self, comment):
+        self.comments.append(comment)
 
-    if (commentText === "") {
-        alert("Please enter a comment.");
-        return;
-    }
+    def display(self):
+        print("Content:", self.content)
+        print("Likes:", self.likes)
+        print("Dislikes:", self.dislikes)
+        print("Comments:")
+        for comment in self.comments:
+            print("- ", comment)
 
-    const commentList = document.getElementById("comment-list");
-    const commentItem = document.createElement("li");
-    commentItem.textContent = commentText;
-    commentList.appendChild(commentItem);
 
-    // Clear the input field
-    commentInput.value = "";
-}
+class SocialMedia:
+    def __init__(self):
+        self.posts = []
 
+    def create_post(self, content):
+        post = Post(content)
+        self.posts.append(post)
+
+    def like_post(self, post_index):
+        if 0 <= post_index < len(self.posts):
+            self.posts[post_index].like()
+        else:
+            print("Invalid post index")
+
+    def dislike_post(self, post_index):
+        if 0 <= post_index < len(self.posts):
+            self.posts[post_index].dislike()
+        else:
+            print("Invalid post index")
+
+    def comment_on_post(self, post_index, comment):
+        if 0 <= post_index < len(self.posts):
+            self.posts[post_index].add_comment(comment)
+        else:
+            print("Invalid post index")
+
+    def display_posts(self):
+        for i, post in enumerate(self.posts):
+            print(f"Post {i}:")
+            post.display()
+            print("\n")
+
+
+# Example usage:
+if __name__ == "__main__":
+    social_media = SocialMedia()
+
+    social_media.create_post("Hello, World!")
+    social_media.create_post("Python is awesome!")
+
+    social_media.like_post(0)
+    social_media.dislike_post(1)
+    social_media.comment_on_post(0, "Great post!")
+    social_media.comment_on_post(1, "I disagree!")
+
+    social_media.display_posts()
