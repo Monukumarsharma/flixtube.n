98a6c47465db1fbd51ff84048cef49bb080bddcd
Monukumarsharma <144675020+Monukumarsharma@users.noreply.github.com>
2023-11-08 16:52:19 +0530

    added

--- server.js
+++ server.js
@@ -1,12 +1,2 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('public')); // Serve static files from the "public" directory
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
 
 
